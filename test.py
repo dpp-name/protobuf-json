fa98cbeb9f564da23f910ce29619bc97a8fe810d
paul.dovbush <paul.dovbush@f7d1fd10-7ea1-11de-8424-ff3729bb81c9>
2010-07-23 14:47:30 +0000

    fix tests

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
-#! /usr/bin/env python
+#!/usr/bin/python
 
-import os, sys
-# set import path to app root
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
-import protobuf_json
-
+import os, sys, json
 from pprint import pprint
 
-
+import protobuf_json
 
 import test_pb2 as pb_test
 
@@ -35,14 +30,11 @@ pb.rep_int.append(2)
 pb.bs.append("\x00\x01\x02\x03\x04");
 pb.bs.append("\x05\x06\x07\x08\x09");
 
-pb.Extensions[pb_test.query]='test ext'
-pb.Extensions[pb_test.long]=53267
-
 # convert it to JSON and back
 pprint(pb.SerializeToString())
-json_str=protobuf_json.pb2json(pb)
-print json_str
-pb2=protobuf_json.json2pb(pb_test.TestMessage, json_str)
+json_obj=protobuf_json.pb2json(pb)
+print json_obj
+pb2=protobuf_json.json2pb(pb_test.TestMessage(), json_obj)
 pprint(pb2.SerializeToString())
 
 if pb == pb2:
--- test_pb2.py
+++ test_pb2.py
@@ -3,15 +3,21 @@
 from google.protobuf import descriptor
 from google.protobuf import message
 from google.protobuf import reflection
-from google.protobuf import service
-from google.protobuf import service_reflection
 from google.protobuf import descriptor_pb2
+# @@protoc_insertion_point(imports)
+
+
+DESCRIPTOR = descriptor.FileDescriptor(
+  name='test.proto',
+  package='protobuf_json_test',
+  serialized_pb='\n\ntest.proto\x12\x12protobuf_json_test\"\xfc\x02\n\x0bTestMessage\x12\n\n\x02id\x18\x01 \x02(\x05\x12\x13\n\x05query\x18\x02 \x01(\t:\x04test\x12\x0c\n\x04\x66lag\x18\x03 \x01(\x08\x12?\n\ttest_enum\x18\x04 \x01(\x0e\x32(.protobuf_json_test.TestMessage.TestEnum:\x02V1\x12=\n\nnested_msg\x18\x05 \x02(\x0b\x32).protobuf_json_test.TestMessage.NestedMsg\x12>\n\x0bnested_msgs\x18\x06 \x03(\x0b\x32).protobuf_json_test.TestMessage.NestedMsg\x12\x0f\n\x07rep_int\x18\x07 \x03(\x05\x12\t\n\x01\x62\x18\x08 \x01(\x0c\x12\n\n\x02\x62s\x18\t \x03(\x0c\x1a\x33\n\tNestedMsg\x12\n\n\x02id\x18\x01 \x02(\x05\x12\r\n\x05title\x18\x02 \x01(\t\x12\x0b\n\x03url\x18\x03 \x01(\t\"\x1a\n\x08TestEnum\x12\x06\n\x02V1\x10\x01\x12\x06\n\x02V2\x10\x02*\x05\x08\x64\x10\xc8\x01:8\n\x05query\x12\x1f.protobuf_json_test.TestMessage\x18\x64 \x01(\t:\x08test ext:-\n\x04long\x12\x1f.protobuf_json_test.TestMessage\x18\x65 \x01(\x04')
+
 
 QUERY_FIELD_NUMBER = 100
 query = descriptor.FieldDescriptor(
   name='query', full_name='protobuf_json_test.query', index=0,
   number=100, type=9, cpp_type=9, label=1,
-  default_value=unicode("test ext", "utf-8"),
+  has_default_value=True, default_value=unicode("test ext", "utf-8"),
   message_type=None, enum_type=None, containing_type=None,
   is_extension=True, extension_scope=None,
   options=None)
@@ -19,7 +25,7 @@ LONG_FIELD_NUMBER = 101
 long = descriptor.FieldDescriptor(
   name='long', full_name='protobuf_json_test.long', index=1,
   number=101, type=4, cpp_type=4, label=1,
-  default_value=0,
+  has_default_value=False, default_value=0,
   message_type=None, enum_type=None, containing_type=None,
   is_extension=True, extension_scope=None,
   options=None)
@@ -27,7 +33,8 @@ long = descriptor.FieldDescriptor(
 _TESTMESSAGE_TESTENUM = descriptor.EnumDescriptor(
   name='TestEnum',
   full_name='protobuf_json_test.TestMessage.TestEnum',
-  filename='TestEnum',
+  filename=None,
+  file=DESCRIPTOR,
   values=[
     descriptor.EnumValueDescriptor(
       name='V1', index=0, number=1,
@@ -38,127 +45,144 @@ _TESTMESSAGE_TESTENUM = descriptor.EnumDescriptor(
       options=None,
       type=None),
   ],
+  containing_type=None,
   options=None,
+  serialized_start=382,
+  serialized_end=408,
 )
 
 
 _TESTMESSAGE_NESTEDMSG = descriptor.Descriptor(
   name='NestedMsg',
   full_name='protobuf_json_test.TestMessage.NestedMsg',
-  filename='test.proto',
+  filename=None,
+  file=DESCRIPTOR,
   containing_type=None,
   fields=[
     descriptor.FieldDescriptor(
       name='id', full_name='protobuf_json_test.TestMessage.NestedMsg.id', index=0,
       number=1, type=5, cpp_type=1, label=2,
-      default_value=0,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='title', full_name='protobuf_json_test.TestMessage.NestedMsg.title', index=1,
       number=2, type=9, cpp_type=9, label=1,
-      default_value=unicode("", "utf-8"),
+      has_default_value=False, default_value=unicode("", "utf-8"),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='url', full_name='protobuf_json_test.TestMessage.NestedMsg.url', index=2,
       number=3, type=9, cpp_type=9, label=1,
-      default_value=unicode("", "utf-8"),
+      has_default_value=False, default_value=unicode("", "utf-8"),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
   ],
   extensions=[
   ],
-  nested_types=[],  # TODO(robinson): Implement.
+  nested_types=[],
   enum_types=[
   ],
-  options=None)
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  serialized_start=329,
+  serialized_end=380,
+)
 
 _TESTMESSAGE = descriptor.Descriptor(
   name='TestMessage',
   full_name='protobuf_json_test.TestMessage',
-  filename='test.proto',
+  filename=None,
+  file=DESCRIPTOR,
   containing_type=None,
   fields=[
     descriptor.FieldDescriptor(
       name='id', full_name='protobuf_json_test.TestMessage.id', index=0,
       number=1, type=5, cpp_type=1, label=2,
-      default_value=0,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='query', full_name='protobuf_json_test.TestMessage.query', index=1,
       number=2, type=9, cpp_type=9, label=1,
-      default_value=unicode("test", "utf-8"),
+      has_default_value=True, default_value=unicode("test", "utf-8"),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='flag', full_name='protobuf_json_test.TestMessage.flag', index=2,
       number=3, type=8, cpp_type=7, label=1,
-      default_value=False,
+      has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='test_enum', full_name='protobuf_json_test.TestMessage.test_enum', index=3,
       number=4, type=14, cpp_type=8, label=1,
-      default_value=1,
+      has_default_value=True, default_value=1,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='nested_msg', full_name='protobuf_json_test.TestMessage.nested_msg', index=4,
       number=5, type=11, cpp_type=10, label=2,
-      default_value=None,
+      has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='nested_msgs', full_name='protobuf_json_test.TestMessage.nested_msgs', index=5,
       number=6, type=11, cpp_type=10, label=3,
-      default_value=[],
+      has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='rep_int', full_name='protobuf_json_test.TestMessage.rep_int', index=6,
       number=7, type=5, cpp_type=1, label=3,
-      default_value=[],
+      has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='b', full_name='protobuf_json_test.TestMessage.b', index=7,
       number=8, type=12, cpp_type=9, label=1,
-      default_value="",
+      has_default_value=False, default_value="",
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     descriptor.FieldDescriptor(
       name='bs', full_name='protobuf_json_test.TestMessage.bs', index=8,
       number=9, type=12, cpp_type=9, label=3,
-      default_value=[],
+      has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
   ],
   extensions=[
   ],
-  nested_types=[],  # TODO(robinson): Implement.
+  nested_types=[_TESTMESSAGE_NESTEDMSG, ],
   enum_types=[
     _TESTMESSAGE_TESTENUM,
   ],
-  options=None)
+  options=None,
+  is_extendable=True,
+  extension_ranges=[(100, 200), ],
+  serialized_start=35,
+  serialized_end=415,
+)
 
 
+_TESTMESSAGE_NESTEDMSG.containing_type = _TESTMESSAGE;
 _TESTMESSAGE.fields_by_name['test_enum'].enum_type = _TESTMESSAGE_TESTENUM
 _TESTMESSAGE.fields_by_name['nested_msg'].message_type = _TESTMESSAGE_NESTEDMSG
 _TESTMESSAGE.fields_by_name['nested_msgs'].message_type = _TESTMESSAGE_NESTEDMSG
+_TESTMESSAGE_TESTENUM.containing_type = _TESTMESSAGE;
 
 class TestMessage(message.Message):
   __metaclass__ = reflection.GeneratedProtocolMessageType
@@ -166,7 +190,12 @@ class TestMessage(message.Message):
   class NestedMsg(message.Message):
     __metaclass__ = reflection.GeneratedProtocolMessageType
     DESCRIPTOR = _TESTMESSAGE_NESTEDMSG
+    
+    # @@protoc_insertion_point(class_scope:protobuf_json_test.TestMessage.NestedMsg)
   DESCRIPTOR = _TESTMESSAGE
+  
+  # @@protoc_insertion_point(class_scope:protobuf_json_test.TestMessage)
 
 TestMessage.RegisterExtension(query)
 TestMessage.RegisterExtension(long)
+# @@protoc_insertion_point(module_scope)
