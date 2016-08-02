e00b25290e102248dc466f5af64a4a69e192dd02
dpp <dpp@dpp.su>
2016-08-02 18:51:45 +0300

    reformat, add useFieldNumber option to serialize with field.number instead of field.name

--- protobuf_json.py
+++ protobuf_json.py
@@ -37,104 +37,112 @@ Provide serialization and de-serialization of Google's protobuf Messages into/fr
 # Note that preservation of unknown fields is currently not available for Python (c) google docs
 # extensions is not supported from 0.0.5 (due to gpb2.3 changes)
 
-__version__='0.0.5'
+__version__='0.0.6'
 __author__='Paul Dovbush <dpp@dpp.su>'
 
 
-import json	# py2.6+ TODO: add support for other JSON serialization modules
+import json     # py2.6+ TODO: add support for other JSON serialization modules
 from google.protobuf.descriptor import FieldDescriptor as FD
-
+from functools import partial
 
 class ParseError(Exception): pass
 
 
-def json2pb(pb, js):
-	''' convert JSON string to google.protobuf.descriptor instance '''
-	for field in pb.DESCRIPTOR.fields:
-		if field.name not in js:
-			continue
-		if field.type == FD.TYPE_MESSAGE:
-			pass
-		elif field.type in _js2ftype:
-			ftype = _js2ftype[field.type]
-		else: 
-			raise ParseError("Field %s.%s of type '%d' is not supported" % (pb.__class__.__name__, field.name, field.type, ))
-		value = js[field.name]
-		if field.label == FD.LABEL_REPEATED:
-			pb_value = getattr(pb, field.name, None)
-			for v in value:
-				if field.type == FD.TYPE_MESSAGE:
-					json2pb(pb_value.add(), v)
-				else:
-					pb_value.append(ftype(v))
-		else:
-			if field.type == FD.TYPE_MESSAGE:
-				json2pb(getattr(pb, field.name, None), value)
-			else:
-				setattr(pb, field.name, ftype(value))
-	return pb
+def json2pb(pb, js, useFieldNumber=False):
+    ''' convert JSON string to google.protobuf.descriptor instance '''
+    for field in pb.DESCRIPTOR.fields:
+        if useFieldNumber:
+            key = field.number
+        else:
+            key = field.name
+        if key not in js:
+            continue
+        if field.type == FD.TYPE_MESSAGE:
+            pass
+        elif field.type in _js2ftype:
+            ftype = _js2ftype[field.type]
+        else:
+            raise ParseError("Field %s.%s of type '%d' is not supported" % (pb.__class__.__name__, field.name, field.type, ))
+        value = js[key]
+        if field.label == FD.LABEL_REPEATED:
+            pb_value = getattr(pb, field.name, None)
+            for v in value:
+                if field.type == FD.TYPE_MESSAGE:
+                    json2pb(pb_value.add(), v, useFieldNumber=useFieldNumber)
+                else:
+                    pb_value.append(ftype(v))
+        else:
+            if field.type == FD.TYPE_MESSAGE:
+                json2pb(getattr(pb, field.name, None), value, useFieldNumber=useFieldNumber)
+            else:
+                setattr(pb, field.name, ftype(value))
+    return pb
 
 
 
-def pb2json(pb):
-	''' convert google.protobuf.descriptor instance to JSON string '''
-	js = {}
-	# fields = pb.DESCRIPTOR.fields #all fields
-	fields = pb.ListFields()	#only filled (including extensions)
-	for field,value in fields:
-		if field.type == FD.TYPE_MESSAGE:
-			ftype = pb2json
-		elif field.type in _ftype2js:
-			ftype = _ftype2js[field.type]
-		else:
-			raise ParseError("Field %s.%s of type '%d' is not supported" % (pb.__class__.__name__, field.name, field.type, ))
-		if field.label == FD.LABEL_REPEATED:
-			js_value = []
-			for v in value:
-				js_value.append(ftype(v))
-		else:
-			js_value = ftype(value)
-		js[field.name] = js_value
-	return js
+def pb2json(pb, useFieldNumber=False):
+    ''' convert google.protobuf.descriptor instance to JSON string '''
+    js = {}
+    # fields = pb.DESCRIPTOR.fields #all fields
+    fields = pb.ListFields()        #only filled (including extensions)
+    for field,value in fields:
+        if useFieldNumber:
+            key = field.number
+        else:
+            key = field.name
+        if field.type == FD.TYPE_MESSAGE:
+            ftype = partial(pb2json, useFieldNumber=useFieldNumber)
+        elif field.type in _ftype2js:
+            ftype = _ftype2js[field.type]
+        else:
+            raise ParseError("Field %s.%s of type '%d' is not supported" % (pb.__class__.__name__, field.name, field.type, ))
+        if field.label == FD.LABEL_REPEATED:
+            js_value = []
+            for v in value:
+                js_value.append(ftype(v))
+        else:
+            js_value = ftype(value)
+        js[key] = js_value
+    return js
 
 
 _ftype2js = {
-	FD.TYPE_DOUBLE: float,
-	FD.TYPE_FLOAT: float,
-	FD.TYPE_INT64: long,
-	FD.TYPE_UINT64: long,
-	FD.TYPE_INT32: int,
-	FD.TYPE_FIXED64: float,
-	FD.TYPE_FIXED32: float,
-	FD.TYPE_BOOL: bool,
-	FD.TYPE_STRING: unicode,
-	#FD.TYPE_MESSAGE: pb2json,		#handled specially
-	FD.TYPE_BYTES: lambda x: x.encode('string_escape'),
-	FD.TYPE_UINT32: int,
-	FD.TYPE_ENUM: int,
-	FD.TYPE_SFIXED32: float,
-	FD.TYPE_SFIXED64: float,
-	FD.TYPE_SINT32: int,
-	FD.TYPE_SINT64: long,
+    FD.TYPE_DOUBLE: float,
+    FD.TYPE_FLOAT: float,
+    FD.TYPE_INT64: long,
+    FD.TYPE_UINT64: long,
+    FD.TYPE_INT32: int,
+    FD.TYPE_FIXED64: float,
+    FD.TYPE_FIXED32: float,
+    FD.TYPE_BOOL: bool,
+    FD.TYPE_STRING: unicode,
+    #FD.TYPE_MESSAGE: pb2json,              #handled specially
+    FD.TYPE_BYTES: lambda x: x.encode('string_escape'),
+    FD.TYPE_UINT32: int,
+    FD.TYPE_ENUM: int,
+    FD.TYPE_SFIXED32: float,
+    FD.TYPE_SFIXED64: float,
+    FD.TYPE_SINT32: int,
+    FD.TYPE_SINT64: long,
 }
 
 _js2ftype = {
-	FD.TYPE_DOUBLE: float,
-	FD.TYPE_FLOAT: float,
-	FD.TYPE_INT64: long,
-	FD.TYPE_UINT64: long,
-	FD.TYPE_INT32: int,
-	FD.TYPE_FIXED64: float,
-	FD.TYPE_FIXED32: float,
-	FD.TYPE_BOOL: bool,
-	FD.TYPE_STRING: unicode,
-	# FD.TYPE_MESSAGE: json2pb,	#handled specially
-	FD.TYPE_BYTES: lambda x: x.decode('string_escape'),
-	FD.TYPE_UINT32: int,
-	FD.TYPE_ENUM: int,
-	FD.TYPE_SFIXED32: float,
-	FD.TYPE_SFIXED64: float,
-	FD.TYPE_SINT32: int,
-	FD.TYPE_SINT64: long,
+    FD.TYPE_DOUBLE: float,
+    FD.TYPE_FLOAT: float,
+    FD.TYPE_INT64: long,
+    FD.TYPE_UINT64: long,
+    FD.TYPE_INT32: int,
+    FD.TYPE_FIXED64: float,
+    FD.TYPE_FIXED32: float,
+    FD.TYPE_BOOL: bool,
+    FD.TYPE_STRING: unicode,
+    # FD.TYPE_MESSAGE: json2pb,     #handled specially
+    FD.TYPE_BYTES: lambda x: x.decode('string_escape'),
+    FD.TYPE_UINT32: int,
+    FD.TYPE_ENUM: int,
+    FD.TYPE_SFIXED32: float,
+    FD.TYPE_SFIXED64: float,
+    FD.TYPE_SINT32: int,
+    FD.TYPE_SINT64: long,
 }
 
